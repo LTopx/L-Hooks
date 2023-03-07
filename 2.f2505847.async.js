4746001a6d94489e158afbc8f7232eb947655552
liushuxiang <276900785@qq.com>
2023-03-07 18:57:59 +0800

    Updates

---
+++ 2.f2505847.async.js
@@ -0,0 +1,1 @@
+(self.webpackChunkl_hooks=self.webpackChunkl_hooks||[]).push([[2],{57002:function(){}}]);
--- hooks/useFetch/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { FetchConfig, FetchReturn } from './types';
-declare function useFetch<T = any>({ url, method, headers, }: FetchConfig): FetchReturn;
-export default useFetch;
